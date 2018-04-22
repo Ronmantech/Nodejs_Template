3b468ca287910b034c1d897e7d4f2af825d2a346
Ronan Manoj <ronmantech@gmail.com>
2018-04-22 16:22:53 -0700

    first

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.set('port', (process.env.PORT || 8080));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response)
+{
+  response.send('Hello World!');
+})
+
+app.listen(app.get('port'), function()
+{
+  console.log("Node app is running at localhost:" + app.get('port'));
+})
