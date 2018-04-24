cfd2ea1e0a10072122b689f225457931609344f9
Ronan Manoj <ronmantech@gmail.com>
2018-04-23 19:37:55 -0700

    updating nodejs_template's Project

--- tests/test.js
+++ tests/test.js
@@ -4,3 +4,12 @@ const assert = require('assert');
 const chai = require('chai');
 const should = chai.should();
 const expect = chai.expect;
+
+// pending; to make 'not' pending, remove the 'x' in front of 'describe'
+xdescribe(' SOMETHING ', () =>
+{
+  it(' SOMETHING ', () =>
+  {
+    // Enter Test code here. 
+  });
+});
--- tools/gulpfile.js
+++ tools/gulpfile.js
@@ -11,9 +11,41 @@ const rename = require('gulp-rename');
 const sass = require('gulp-sass');
 const inject = require('gulp-inject-string');
 
+// example
+gulp.task('imgCompress', () =>
+{
+  gulp.src('../src/img/FINALNAME.png')
+     .pipe(imagemin({ optimizationLevel: 5 }))
+     .pipe(gulp.dest('../build/img'))
+});
 
+// example
+gulp.task("concatStyle", () =>
+{
+   gulp.src("../src/scss/style.scss")
+      .pipe(concat("FILENAME.scss"))
+      .pipe(gulp.dest("../src/scss"));
+});
 
+// example
+gulp.task("jscompress", () =>
+{
+   gulp.src('../src/js/FILENAME.js')
+      .pipe(uglify().on('error', gutil.log)) // error outputs to console.
+      // .pipe(uglify())
+      .pipe(rename('FILENAME.min.js'))
+      .pipe(gulp.dest('../build/js'));
+});
 
+// example
+gulp.task("compileSass", () =>
+{
+   gulp.src("../src/scss/FILENAME.scss")
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(rename('deter.min.css'))
+    .pipe(gulp.dest('../build/css'))
+});
 
+gulp.task("build", gulpSequence(['jscompress', 'concatStyle', 'imgCompress'], 'compileSass'));
 
 gulp.task("default", ['build']);
