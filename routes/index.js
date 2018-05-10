ed0fb623df3f555deec9c7148a1be16c582363ac
Ronan Manoj <ronmantech@gmail.com>
2018-05-10 16:03:41 -0700

    updating nodejs's Project

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ let express = require('express');
 let router = express.Router();
 
 // GET 
-router.get('/', function(req, res, next)
+router.get('*', function(req, res, next)
 {
   return res.write("Hello, World!");
   // return res.render('PUG file name ', { title: ' TITLE NAME HERE' });
